8b6db6199eba8b780bfee4639436ef2712ca27b4
Vincenzo Sicurella <sicurella12@gmail.com>
2022-04-01 20:09:34 -0400

    hack for changing the key blend mode in the console

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-import { refreshColor, renderToCanvas, resetKeys } from './src/draw.js'
+import { resetKeys, setKeyBlendMode, setShadingBlendMode } from './src/draw.js'
 import { currentLtn } from './src/ltn.js'
 
 document.body.onload = () => {
-     resetKeys()
-     refreshColor();
-     renderToCanvas();
+     resetKeys();
 };
 document.getElementById("ltn-file").onchange = (function() {
     currentLtn.loadFile(this.files[0]);
 });
+
+window.setKeyBlendMode = setKeyBlendMode;
+window.setShadingBlendMode = setShadingBlendMode;
