9deda67e6ce1108bc7d4f438a65191b1014e5240
Vincenzo Sicurella <sicurella12@gmail.com>
2022-04-26 00:34:45 -0400

    dot and hex guides

--- src/image.js
+++ src/image.js
@@ -1,4 +1,4 @@
-import { Board, boardGeometry, calculateCentres, LATERALRADIUSRATIO, LumatoneBaseImage } from "./hex";
+import { Board, LumatoneBaseImage } from "./hex";
 
 import { fabric } from "fabric";
 import { rgbaToString, hexToRgba, getRgbLed } from "./color.js";
@@ -9,26 +9,92 @@ export class ImageToLtnConverter {
     currentImage = new Image();
 
     samplePoints = [];
+    dotGroup = null;
+    hexGroup = null;
+
+    sampleW = 0;
+    sampleH = 0;
 
     constructor(canvasId, width, height) {
         this.canvasId = canvasId;
         this.width = width;
         this.height = height;
 
-        this.canvas = new fabric.Canvas(canvasId, { width: this.width, height: this.height });
+        this.canvas = new fabric.Canvas(canvasId, { width: this.width, height: this.height, preserveObjectStacking: true });
 
         fabric.Image.fromURL(require("../png/keyboard-base-1x.png"), (img) => {
             this.backgroundImage = img;
             this.backgroundImage.scaleToHeight(this.height)
             this.resetCanvas();
+
+            this.board = new Board(0, 5);
+
+            const imgWidth = this.backgroundImage.width * this.backgroundImage.scaleX;
+            const imgHeight = this.backgroundImage.height * this.backgroundImage.scaleY;
+            LumatoneBaseImage.setBoardBasis(this.board, imgWidth, height, 0, 0);
+
+            this.sampleW = imgWidth * LumatoneBaseImage.keyW * 0.5;
+            this.sampleH = imgHeight * LumatoneBaseImage.keyH * 0.5;
+    
+            this.resetSamplePoints();
+            this.addHexGuides();
         });
+    }
 
-        this.board = new Board(0, 5);
-        LumatoneBaseImage.setBoardBasis(this.board);
+    resetSamplePoints() {
+        this.samplePoints = [];
 
-        this.addHexGuides();
+        let dots = [];
+        let hexes = [];
 
-        console.log(this.board);
+        const rotation = 0.28;
+
+        let hexPoints = [];
+        for (var i = 0; i < 6; i++) {
+            const theta = 2 * Math.PI / 6 * i + rotation;
+            hexPoints.push({ 
+                x: Math.cos(theta) * this.sampleW,
+                y: Math.sin(theta) * this.sampleH 
+            });
+        }
+
+        const octaveCentres = this.board.getCentres();
+        for (const oct in octaveCentres) {
+            for (const key in octaveCentres[oct]) {
+                const point = octaveCentres[oct][key];
+                this.samplePoints.push(point);
+
+                const dotRadius = 1;
+                const dot = new fabric.Circle({
+                    radius: dotRadius,
+                    fill: 'red',
+                    left: point.x - dotRadius,
+                    top: point.y - dotRadius
+                });
+
+                dots.push(dot);
+
+                    
+                const hexCoords = hexPoints.map((hp) => new Object({ x: hp.x + point.x, y: hp.y + point.y }));
+                console.log(hexCoords);
+
+                const hex = new fabric.Polygon(hexCoords, {
+                    stroke: 'red',
+                    fill: 'transparent'
+                });
+
+                hexes.push(hex);
+            }
+        }
+
+        const groupOptions = { 
+            evented: false,
+            selectable: false
+         };
+
+        this.dotGroup = new fabric.Group(dots, groupOptions);
+        this.hexGroup = new fabric.Group(hexes, groupOptions);
+        
     }
 
     resetCanvas() {
@@ -58,31 +124,13 @@ export class ImageToLtnConverter {
 
                 this.addHexGuides();
             };
-
         };
     
         file.readAsDataURL(this.fileData);
     }
 
     addHexGuides() {
-        const dotRadius = 8;
-        const octaveCentres = this.board.getCentres();
-        for (const oct in octaveCentres) {
-            const centres = octaveCentres[oct];
-            for (const key in centres) {
-                const p = centres[key];
-                let dot = new fabric.Circle({
-                    radius: dotRadius,
-                    fill: 'black',
-                    left: p.x - dotRadius,
-                    top: p.y - dotRadius
-                });
-                this.canvas.add(dot);
-            }
-        }
-    }
-
-    parseTransform() {
-
+        this.canvas.add(this.dotGroup);
+        this.canvas.add(this.hexGroup);
     }
 }
