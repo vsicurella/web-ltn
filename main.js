5113dbb8551a7a919607623d1c7a4c76d828bdb0
Vincenzo Sicurella <sicurella12@gmail.com>
2022-04-28 18:45:44 -0400

    reverted hacky stuff from the wip image branch

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ import { convertImageToLtn, ImageToLtnConverter } from './src/image.js'
 const converter = new ImageToLtnConverter('scale-image', 1300, 280);
 
 document.body.onload = () => {
-    //  resetKeys();
+     resetKeys();
 };
-// document.getElementById("ltn-file").onchange = (function() {
-//     currentLtn.loadFile(this.files[0]);
-// });
+document.getElementById("ltn-file").onchange = (function() {
+    currentLtn.loadFile(this.files[0]);
+});
 
-document.getElementById("ltn-image").onchange = (function() {
-    console.log(this)
-    converter.setImageFile(this.files[0])
-})
+// document.getElementById("ltn-image").onchange = (function() {
+//     console.log(this)
+//     converter.setImageFile(this.files[0])
+// })
 
-// window.setKeyBlendMode = setKeyBlendMode;
-// window.setShadingBlendMode = setShadingBlendMode;
+window.setKeyBlendMode = setKeyBlendMode;
+window.setShadingBlendMode = setShadingBlendMode;
